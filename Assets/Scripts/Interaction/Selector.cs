5ea679faf7023896cd6b0cd6a3e6827e376ef7fe
colo6299 <rocketblox@gmail.com>
2021-07-14 07:45:04 -0700

    add selector functionality and prefabs

--- Assets/Scripts/Interaction/Selector.cs
+++ Assets/Scripts/Interaction/Selector.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 /// <summary>
 /// Selector manages the selection tool.
 /// </summary>
-public class Selector : MonoBehaviour 
+public class Selector : Grabbable
 {
-    private Selectable touchedSelectable;
+    public Selectable selectedObject;
+    public Selectable touchedSelectable;
     private float minSqrDistance; //a fast way to store the ~distance of the closest selectable
     /// <summary>
     /// When called, returns the object the selector is most happy with selecting. This is the thing you call to get the item/unit you're pointing at
@@ -19,6 +20,18 @@ public class Selector : MonoBehaviour
         {
             //If there is no collectable touching the collider, 
             //raycast to search for a valid selectable.
+            RaycastHit hit;
+            Physics.Raycast(transform.position, transform.forward, out hit, 10f, 1 << 6);
+            Debug.Log(hit.rigidbody.name);
+            if(hit.rigidbody != null)
+            {
+                Selectable select = hit.rigidbody.gameObject.GetComponent<Selectable>();
+                if (select != null)
+                {
+                    Debug.Log(select.name);
+                    return select;
+                }
+            }
         }
         else
         {
@@ -28,6 +41,13 @@ public class Selector : MonoBehaviour
         return null;
     }
 
+    public override void Trigger(bool triggerState)
+    {
+        if (triggerState)
+        {
+            selectedObject = Select();
+        }
+    }
     void OnTriggerStay(Collider other)
     {
         float squareDistance = (other.transform.position - transform.position).sqrMagnitude; //finds the x, y, z distances by subtracting, then finds the 

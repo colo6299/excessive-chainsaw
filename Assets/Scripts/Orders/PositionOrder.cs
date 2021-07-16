36c9a170a0b55e11068c87f058325a27a2efcec3
colo6299 <rocketblox@gmail.com>
2021-07-16 06:53:06 -0700

    update debug rays

--- Assets/Scripts/Orders/PositionOrder.cs
+++ Assets/Scripts/Orders/PositionOrder.cs
@@ -6,15 +6,17 @@ public class PositionOrder : Order
 {
     protected Vector3 storedPos;
     public Transform rayPos;
+    private RaycastHit hit;
     public override void Trigger(bool triggerState)
     {
         if (!triggerState) 
         {
             Debug.DrawRay(rayPos.position, rayPos.forward, Color.red);
+            Debug.DrawRay(hit.point, hit.normal, Color.green);
             return; 
         }
         //Raycast position and store it
-        RaycastHit hit;
+        
         Physics.Raycast(rayPos.position, rayPos.forward, out hit, 100f, 1 << 0);
         Debug.DrawRay(hit.point, hit.normal, Color.green);
         Debug.DrawRay(rayPos.position, rayPos.forward, Color.green);

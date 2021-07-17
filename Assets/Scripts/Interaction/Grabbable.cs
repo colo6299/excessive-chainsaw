2b434812f098a764cddd4f9fef34f18dd0b93264
colo6299 <rocketblox@gmail.com>
2021-07-17 07:58:00 -0700

    add grabbable control states

--- Assets/Scripts/Interaction/Grabbable.cs
+++ Assets/Scripts/Interaction/Grabbable.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Grabbable : MonoBehaviour
 {
-    
+
     public virtual void Trigger(bool triggerState) //a function that can be overwritten, but is guaranteed to be on any grabbable object.
+    {
+
+    }
+
+    public virtual void TriggerUp()
+    {
+
+    }
+
+    public virtual void TriggerDown()
     {
 
     }
